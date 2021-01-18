944c02266c3c86099b9aabc6d13e3cf8f6707792
Dylano Brouwer <99064035@mydavinci.nl>
2021-01-18 12:10:51 +0100

    commit 1 18/1/2021
    begin gemaakt

---
+++ script.js
@@ -0,0 +1,12 @@
+//body
+document.body.style.backgroundColor="#333333";
+
+//div
+var div=document.createElement("DIV");
+div.id="container";
+div.style.width = "900px";
+div.style.height = "400px";
+div.style.backgroundColor="#b3b3ff";
+div.style.margin="0 auto";
+document.body.appendChild(div);
+
